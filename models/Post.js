b416d1f56e31d42a229a037a89978baa09e04272
Robert Pliszka <robplisz@gmail.com>
2022-06-06 18:35:57 -0400

    Fixed Post model and added price

--- models/Post.js
+++ models/Post.js
@@ -22,6 +22,10 @@ Post.init(
         isURL: true,
       },
     },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
