53e31f0c1444d23fd2b60913296345f519b3fad8
Robert Pliszka <robplisz@gmail.com>
2022-06-06 18:27:22 -0400

    Fixed seed file it works now

--- models/Post.js
+++ models/Post.js
@@ -1,42 +1,41 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
 Post.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        image_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isURL: true
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
-    }
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
 );
 
 module.exports = Post;
--- seeds/reviews-seeds.js
+++ seeds/reviews-seeds.js
@@ -1,33 +1,36 @@
-const { Review } = require("../../models");
+const { Review } = require("../models");
 
-const reviewData = [
-    {
-        review_text: "Nunc rhoncus dui vel sem.",
-        user_id: 1,
-        post_is:1
-    },
-    {
-        review_text: "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
-        user_id: 2,
-        post_is:1
-    },
-    {
-        review_text: "Aliquam erat volutpat. In congue.",
-        user_id: 1,
-        post_is: 2
-    },
-    {
-        review_text: "Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
-        user_id: 3,
-        post_is: 2
-    },
-    {
-        review_text: "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
-        user_id: 1,
-        post_is: 2
-    },
-]
+const reviewdata = [
+  {
+    review_text: "Nunc rhoncus dui vel sem.",
+    user_id: 1,
+    post_id: 1,
+  },
+  {
+    review_text:
+      "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+    user_id: 2,
+    post_id: 1,
+  },
+  {
+    review_text: "Aliquam erat volutpat. In congue.",
+    user_id: 1,
+    post_id: 2,
+  },
+  {
+    review_text:
+      "Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+    user_id: 3,
+    post_id: 2,
+  },
+  {
+    review_text:
+      "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
+    user_id: 1,
+    post_id: 2,
+  },
+];
 
-const seedReviews = () => Comment.bulkCreate(reviewdata);
+const seedReviews = () => Review.bulkCreate(reviewdata);
 
 module.exports = seedReviews;
