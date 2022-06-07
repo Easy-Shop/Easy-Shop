415fddfea8b69b4fb381280ebca6338ba96c9125
Robert Pliszka <robplisz@gmail.com>
2022-06-07 17:22:35 -0400

    Created login page in handlebars, added login JS to have login work

---
+++ public/javascript/login.js
@@ -0,0 +1,28 @@
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#loginUsername").value.trim();
+  const password = document.querySelector("#loginPassword").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // Redirects to Dashboard page
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
