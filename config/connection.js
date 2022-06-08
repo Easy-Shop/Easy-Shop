61a1ef15bc1f47ea288af463677ef5c4c9ead7dd
Robert Pliszka <robplisz@gmail.com>
2022-06-08 18:10:10 -0400

    Added styles to login page

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,16 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize("easy_shop_db", "root", process.env.DB_PASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
 
 module.exports = sequelize;
